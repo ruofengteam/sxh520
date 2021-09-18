61dab36e32352111b5a4c225742d35f6cbfc62e8
lancelot <lancelotzx@163.com>
2021-09-19 01:27:49 +0800

    [feature]第一次提交vant初始化框架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <van-goods-action>
+      <van-goods-action-icon icon="chat-o" >
+        客服
+      </van-goods-action-icon>
+      <van-goods-action-icon icon="cart-o" >
+        购物车
+      </van-goods-action-icon>
+      <van-goods-action-button type="warning">
+        加入购物车
+      </van-goods-action-button>
+      <van-goods-action-button type="danger" >
+        立即购买
+      </van-goods-action-button>
+    </van-goods-action>
+    <van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
   </div>
 </template>
 
 <script>
+import { GoodsAction,
+  GoodsActionIcon,
+  GoodsActionButton ,
+  Cell, CellGroup} from 'vant';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+     [GoodsAction.name]: GoodsAction,
+    [GoodsActionIcon.name]: GoodsActionIcon,
+    [GoodsActionButton.name]: GoodsActionButton,
+    [CellGroup.name]: CellGroup,
+    [Cell.name]: Cell
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import  'vant/lib/index.css';
 
 Vue.config.productionTip = false
 
